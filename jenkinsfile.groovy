808cae5e35072704b52f23018c751b9d9d10d4bb
mahesh <maheshryali@gmail.com>
2022-10-16 16:22:22 +0530

    changes1

---
+++ jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent { label 'SHOPIZER' }                  
+    stages {
+        stage('vcs') {
+            steps {
+                git branch: 'developbranch',
+                       url: 'https://github.com/maheshryali/shopizer.git'
+            }
+        }
+        stage('maven build') {
+            steps {
+                sh ("mvn package")
+                }
+        }
+        stage( 'artifacts') {
+            steps{
+            junits '**/surefire-reports/*.xml'
+            }
+        }
+    }
+}
